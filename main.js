0a15ad129a6cc0151e86441130f392aab1159f16
Michael Chop <007micle007@gmail.com>
2022-11-07 11:36:08 +0300

    Added a menu bar to the application

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const {app, BrowserWindow} = require("electron");
+const {app, BrowserWindow, Menu} = require("electron");
 
 const isDev = process.env.NODE !== 'production'
 const isMac = process.platform === 'darwin'
@@ -19,8 +19,25 @@ function createMainWindow() {
     mainWindow.loadFile(path.join(__dirname, './render/index.html'))
 }
 
+// Create About window
+function createAboutWindow() {
+    const aboutWindow = new BrowserWindow({
+        title: 'About Image Resizer',
+        width: 300,
+        height: 300,
+        autoHideMenuBar: true
+    });
+
+    aboutWindow.loadFile(path.join(__dirname, './render/about.html'))
+}
+
 app.whenReady().then(() => {
     createMainWindow()
+
+    // Implement menu
+    const mainMenu = Menu.buildFromTemplate(menu)
+    Menu.setApplicationMenu(mainMenu)
+
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
@@ -28,6 +45,29 @@ app.whenReady().then(() => {
         }
     })
 })
+
+//Menu template
+const menu = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Quit',
+                click: () => app.quit(),
+                acceleration: 'Ctrl+W'
+            }
+        ]
+    },
+    {
+        label: 'Help',
+        submenu: [
+            {
+                label: 'About',
+                click: createAboutWindow
+            }
+        ]
+    }
+]
 
 app.on('window-all-closed', () => {
     if (!isMac) {
